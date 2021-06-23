8a5c22f22e3bff2039e226168629c6ebe714b01a
Ryan Cooley <ryan.cooley@processmaker.com>
2021-06-23 15:01:07 -0700

    Refactor controller

--- src/Http/Controllers/AdoaMigrateUsersController.php
+++ src/Http/Controllers/AdoaMigrateUsersController.php
@@ -3,31 +3,24 @@
 namespace ProcessMaker\Package\Adoa\Http\Controllers;
 use ProcessMaker\Http\Controllers\Controller;
 use ProcessMaker\Http\Resources\ApiCollection;
-use ProcessMaker\Adoa\classes\MigrateUsersProd;
-use ProcessMaker\Adoa\classes\MigrateUsersDev;
+use ProcessMaker\Package\Adoa\Jobs\MigrateUsers;
 use ProcessMaker\Adoa\classes\MigrateAdministrators;
 use RBAC;
 use Illuminate\Http\Request;
 use URL;
-use \DateTime;
-use \DB;
 
 class AdoaMigrateUsersController extends Controller
 {
     public function migratedUsersProd()
     {
-        require_once dirname(__DIR__, 3) . '/classes/MigrateUsersProd.php';
-        $migrateUsers = new MigrateUsersProd();
-        $result = $migrateUsers->migrateUserInformation();
-        return ['result' => $result];
+        MigrateUsers::dispatch();
+        return response(['status' => true], 201);
     }
 
     public function migratedUsersDev()
     {
-        require_once dirname(__DIR__, 3) . '/classes/MigrateUsersDev.php';
-        $migrateUsers = new MigrateUsersDev();
-        $result = $migrateUsers->migrateUserInformation();
-        return ['result' => $result];
+        MigrateUsers::dispatch();
+        return response(['status' => true], 201);
     }
 
     public function migrateAdministrators()
